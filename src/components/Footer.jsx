71865b19cafb85784c9f455620f92dc1128d177e
pablozuta <pablo.zuta.h@gmail.com>
2023-04-23 02:26:03 -0400

    se agrega footer.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,27 @@
 import React from "react";
-import GreenBtn from "./GreenBtn";
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div className="w-full bg-gray-900 text-gray-400 py-8 px-2 border-t-2">
-      <div className="flex flex-col items-center justify-center max-w-screen-xl mx-auto h-full px-3 py-12">
-        <div className="flex flex-col gap-8 md:flex-row items-center justify-center w-full">
-
-
-          {/* newsletter */}
-          <div className="w-3/4 md:w-1/2">
-            <div className="w-full pt-8 md:pt-2">
-              <p className="font-bold uppercase mb-8 text-lightColor text-lg text-center md:text-left">
-                Subscribe to our newsletter
-              </p>
-              <form
-                action="https://getform.io/f/fbeaf56f-5d61-4e09-9ea2-84dc28c66a69"
-                method="POST"
-                className="flex flex-col sm:flex-row"
-              >
-                <input
-                  type="text"
-                  name="email"
-                  className="w-full p-2 mr-4 rounded-md mb-4 text-gray-900"
-                  placeholder="Daily newsletter..."
-                />
-                <GreenBtn title="Subscribe" className="p-2 mb-4" />
-              </form>
-            </div>
-          </div>
-        </div>
+    <footer className="footer-container bg-gray-900">
+      <div>
+        <p className="font-bold uppercase mb-8 text-lightColor text-lg text-center md:text-left">
+          Subscribe to our newsletter
+        </p>
       </div>
-    </div>
+
+      <div >
+        <form >
+          <input
+            type="text"
+            name="email"
+            className="input-field  p-2 mr-4 rounded-md mb-4 text-gray-900"
+            placeholder="Daily newsletter..."/>
+          <center><button className="submit-footer">Submit</button></center>          
+        </form>
+      </div>
+
+    </footer>
   );
 };
 
